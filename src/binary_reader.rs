4be68314a16f9a68cac6afcb8f072ce5b7c49229
syoch <hacksyoch@gmail.com>
2022-04-21 11:59:05 +0900

    fix binary reader

--- src/binary_reader.rs
+++ src/binary_reader.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq)]
 pub enum Endian {
     Big,
     Little,
@@ -21,17 +22,23 @@ impl BinaryReader {
         }
     }
 
-    fn _join_number<T>(&self, high: T, low: T, shift: usize) {
-        let high = high as usize;
-        let low = low as usize;
+    fn _concat<T>(&self, high: T, low: T, shift: u8) -> u128
+    where
+        T: num::ToPrimitive,
+        T: std::ops::Shr<usize, Output = T>,
+    {
+        let high = high.to_u128().unwrap();
+        let low = low.to_u128().unwrap();
 
         if self.endian == Endian::Big {
-            return (high << shift) | low;
+            (high << shift) | low
         } else {
-            return (low << shift) | high;
+            (low << shift) | high
         }
     }
+}
 
+impl BinaryReader {
     pub fn read_u8(&mut self) -> u8 {
         let result = self.data[self.offset];
         self.offset += 1;
@@ -41,32 +48,34 @@ impl BinaryReader {
     pub fn read_u16(&mut self) -> u16 {
         let high = self.read_u8() as u16;
         let low = self.read_u8() as u16;
-        self._join_number(high, low, 8) as u16
+        self._concat(high, low, 8) as u16
     }
 
     pub fn read_u32(&mut self) -> u32 {
         let high = self.read_u16() as u32;
         let low = self.read_u16() as u32;
-        self._join_number(high, low, 16) as u32
+        self._concat(high, low, 16) as u32
     }
 
     pub fn read_u64(&mut self) -> u64 {
         let high = self.read_u32() as u64;
         let low = self.read_u32() as u64;
-        self._join_number(high, low, 32) as u64
+        self._concat(high, low, 32) as u64
     }
 
     pub fn read_u128(&mut self) -> u128 {
         let high = self.read_u64() as u128;
         let low = self.read_u64() as u128;
-        self._join_number(high, low, 64) as u128
+        self._concat(high, low, 64) as u128
     }
+}
 
+impl BinaryReader {
     pub fn read_half(&mut self) -> u32 {
         if self.is_64bit {
             self.read_u32()
         } else {
-            self.read_u16()
+            self.read_u16() as u32
         }
     }
 
@@ -74,7 +83,7 @@ impl BinaryReader {
         if self.is_64bit {
             self.read_u64()
         } else {
-            self.read_u32()
+            self.read_u32() as u64
         }
     }
 
@@ -82,7 +91,7 @@ impl BinaryReader {
         if self.is_64bit {
             self.read_u128()
         } else {
-            self.read_u64()
+            self.read_u64() as u128
         }
     }
 }
